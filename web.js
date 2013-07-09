0f314628349a70f30edfdee047db960ff1b8e0b3
reggiemack <reggiemack.310@gmail.com>
2013-07-09 16:07:00 +0000

    Hello World from index.html

--- web.js
+++ web.js
@@ -1,12 +1,30 @@
+
 var express = require('express');
+var fs      = requure('fs')
+var buf     = new buffer(30)
+var html    = ""
+
+fs.readFileSync('index.html',,function(err, buf))
+    {
+    if (err)
+       {
+       console.log(err);
+       html = err;
+       }
+    else
+       {
+       html = buf.toString();
+       console.log(html);
+       }
+    }
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(html);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
